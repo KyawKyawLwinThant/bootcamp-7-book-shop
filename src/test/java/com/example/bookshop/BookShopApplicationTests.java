6cd26c5cf43a2b72c466799c781260b9155b36e9
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-01-08 11:21:37 +0630

    initial commit

---
+++ src/test/java/com/example/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
