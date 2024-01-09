09d715039cf1809ce67e305c1860a9d47d2e73e2
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-01-09 10:32:54 +0630

    template init

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
@@ -23,7 +24,7 @@ public class BookShopApplication {
     private final BookDao bookDao;
     private final GenreDao genreDao;
     private final PublisherDao publisherDao;
-    @Bean @Transactional
+    @Bean @Transactional @Profile("data")
     public ApplicationRunner runner(){
         return  r ->{
             Author author1=
