4f6ae40215e20acd5e3ad3495377efbba32368f6
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-01-10 10:30:15 +0630

    book details

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -1,22 +1,60 @@
 package com.example.bookshop.controller;
 
 import com.example.bookshop.dao.BookDao;
+import com.example.bookshop.entity.BookId;
 import com.example.bookshop.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/book")
 public class BookController {
 
     private final BookService bookService;
 
+    //book/list-books
+    @GetMapping("/list-books")
+    public String listBooks(Model model){
+        model.addAttribute("books",
+                bookService.listBooks());
+        return "listbooks";
+    }
+    //book/book-details?id=1&isbn=ISBN-1234
+    @GetMapping("/book-details")
+    public String bookDetails(@RequestParam("id")Integer id,
+                              @RequestParam("isbn")String isbn,Model model){
+        BookId bookId=null;
+        if(Objects.nonNull(id) && Objects.nonNull(isbn)){
+             bookId=new BookId();
+            bookId.setId(id);
+            bookId.setIsbn(isbn);
+        }
+        else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        model.addAttribute("book",
+                bookService.findBookById(bookId));
+        return "bookdetails";
+    }
 
-//    public String listBooks(Model model){
-//        model.addAttribute("books",
-//                bookService.listBooks());
-//
-//    }
-//
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/bookshop/service/BookService.java
+++ src/main/java/com/example/bookshop/service/BookService.java
@@ -3,6 +3,8 @@ package com.example.bookshop.service;
 
 import com.example.bookshop.dao.BookDao;
 import com.example.bookshop.entity.Book;
+import com.example.bookshop.entity.BookId;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,11 @@ public class BookService {
 
     public List<Book> listBooks(){
         return bookDao.findAll();
+    }
+
+    public Book findBookById(BookId bookId){
+        return bookDao.findById(bookId)
+                .orElseThrow(EntityNotFoundException::new);
     }
 
 
