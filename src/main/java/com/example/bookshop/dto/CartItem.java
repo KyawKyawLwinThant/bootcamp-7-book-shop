6c332bc19e85b1416fef3d139cbaa372071bdecf
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2024-01-10 14:02:34 +0630

    cart add

--- src/main/java/com/example/bookshop/dto/CartItem.java
+++ src/main/java/com/example/bookshop/dto/CartItem.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,4 +18,17 @@ public class CartItem {
     private String title;
     private double price;
     private int quantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return id == cartItem.id && Objects.equals(isbn, cartItem.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn);
+    }
 }
--- src/main/java/com/example/bookshop/service/CartBean.java
+++ src/main/java/com/example/bookshop/service/CartBean.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @SessionScope
 public class CartBean {
-    private List<CartItem> cartItems=
-            new ArrayList<>();
+    private Set<CartItem> cartItems=
+            new HashSet<>();
 
     public void addCartItem(CartItem cartItem){
         cartItems.add(cartItem);
@@ -20,4 +22,6 @@ public class CartBean {
     public Integer cartSize(){
         return cartItems.size();
     }
+
+
 }
